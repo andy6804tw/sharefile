2f00d3407f51cc53dc0310cbbfe4cf48ff79d4b9
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2018-04-25 16:40:01 +0800

    fix cli

--- main.js
+++ main.js
@@ -1,13 +1,1 @@
-var http = require('http');
-
-var finalhandler = require('finalhandler');
-var serveStatic = require('serve-static');
-
-var serve = serveStatic("./");
-
-var server = http.createServer(function (req, res) {
-  var done = finalhandler(req, res);
-  serve(req, res, done);
-});
-
-server.listen(8000);
+console.log('Hello, world!');
