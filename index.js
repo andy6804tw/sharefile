eba6120acd5e393751940a041622cff86848deca
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2018-04-25 16:07:34 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+//透過http模組啟動web server服務
+const http = require('http')
+const fs = require('fs')
+const localtunnel = require('localtunnel');
+
+const PORT = 3000;
+const server = http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end(fs.readFileSync('./' + decodeURI(req.url)));
+})
+
+
+if (process.argv[2] != undefined) {
+
+  // //設定服務監聽localhost:3000(127.0.0.1/:3000)
+  server.listen(PORT);
+  // //qrcode.generate(something, { small: true });
+  
+  try {
+    var tunnel = localtunnel(PORT, function (err, tunnel) {
+      console.log(tunnel.url);
+      const something = tunnel.url + '/' + encodeURI(process.argv[2]);
+      console.log(something);
+    });
+  }
+  catch (err) {
+    console.log('Error!');
+  }
+
+} else {
+  console.log('usage:')
+  console.log('       share [file ..]       be sure to your file ')
+}
+
+
+
+
