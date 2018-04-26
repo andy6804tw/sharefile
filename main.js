35ab113af6494cfe99053446d5d82594c77d5725
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2018-04-26 11:41:06 +0800

    solve robot & favicon error

--- main.js
+++ main.js
@@ -4,13 +4,9 @@ const fs = require('fs');
 const localtunnel = require('localtunnel');
 
 const PORT = 3000;
-const server = http.createServer(function (req, res) {
-  console.log(req.url);
-  if (req.url != '/favicon.ico' && req.url != '/robots.txt') {
-    res.writeHead(200);
-    res.end(fs.readFileSync('./' + decodeURI(req.url)));
-  }
-})
+const server =http.createServer(function (req, res) {
+  
+}).listen(PORT);
 
 
 if (process.argv[2] != undefined) {
@@ -18,7 +14,7 @@ if (process.argv[2] != undefined) {
   // //設定服務監聽localhost:3000(127.0.0.1/:3000)
   server.listen(PORT);
   // //qrcode.generate(something, { small: true });
-  
+
   try {
     var tunnel = localtunnel(PORT, function (err, tunnel) {
       const something = tunnel.url + '/' + encodeURI(process.argv[2]);
@@ -33,7 +29,3 @@ if (process.argv[2] != undefined) {
   console.log('usage:');
   console.log('       share [file ..]       be sure to your file ');
 }
-
-
-
-
