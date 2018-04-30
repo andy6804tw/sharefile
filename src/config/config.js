b3ebf991e98e3c8130a75388659b3b0517f0a7c5
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2018-04-30 10:14:34 +0800

    remove dotenv lib

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,2 @@
-// require and configure dotenv, will load vars in .env in process.env
-require('dotenv').config();
 
-module.exports = { port: process.env.PORT };
+module.exports = { port: 30952 };
